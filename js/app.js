2e153f7dc6dfbf713d9f5b6c39f8e9b522c1508c
zhangtreefish <zhangtreefish@users.noreply.github.com>
2015-08-02 16:04:27 -0500

    feat(index.html.js):add catList and bind the click event

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
-var initialCats = [{
-	clickCount:0,
-	name:'cat1',
-	imgSrc:'http://placekitten.com/g/200/300',
-	nickNames:[{nickname:'maomao'},{nickname:'snow-shoes'}]
-},{}];
+var initialCats = [
+	{
+		clickCount:0,
+		name:'CutiePie',
+		imgSrc:'http://placekitten.com/g/200/300',
+		nickNames:[{nickname:'maomao'},{nickname:'snow-shoes'}]
+	},
+	{
+		name:'Zoro',
+		imgSrc: "http://i110.photobucket.com/albums/n114/corsiphoto/tumblr_m00l5xLSbi1qjc1a7o1_500.jpg",
+		clickCount:0,
+		nickNames:[{nickname:'MAOMI'},{nickname:'FURRY'}]
+	},
+	{
+		name:'Strider',
+		imgSrc: 'https://www.kasandbox.org/programming-images/misc/cat-walk.gif',
+		clickCount:0,
+		nickNames:[{nickname:'dark night'},{nickname:'speed'}]
+	}
+];
 
 var Cat = function(data) {
 	this.clickCount = ko.observable(data.clickCount);
@@ -41,6 +55,9 @@ var ViewModel = function() {
 	this.currentCat = ko.observable(this.catList()[0]);//need()
 	this.incrementCounter = function() {
 		self.currentCat().clickCount(self.currentCat().clickCount()+1);
+	};
+	this.catClicked = function(theClicked) {
+		self.currentCat(theClicked);
 	};
 };
 
